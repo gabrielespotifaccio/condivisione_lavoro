2f57ed89ac95f2812309e652af694399248efcd4
gabrielespotifaccio <marchianogabri@gmail.com>
2025-04-12 23:54:32 +0200

    Update add-job.js

--- js/add-job.js
+++ js/add-job.js
@@ -1,1 +1,10 @@
-
+document.getElementById("jobForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const jobData = {
+        title: document.getElementById("jobTitle").value,
+        company: document.getElementById("company").value,
+        address: document.getElementById("address").value,
+        description: document.getElementById("description").value
+    };
+    window.location.href = "index.html";
+});
